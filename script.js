174d34200faccf9a65533cffbe7b2e29bd29d58e
Enrico Meluziis <enricomlz@hotmail.it>
2025-04-24 12:12:11 +0200

    calculates and displays balance summary

--- script.js
+++ script.js
@@ -80,6 +80,16 @@ const calcDisplayBalance = function (movements) {
 
 calcDisplayBalance(account1.movements);
 
+const calcDisplaySummary = function (movements) {
+  const incomes = movements.filter((mov) => mov > 0).reduce((acc, mov) => acc + mov, 0);
+  const out = movements.filter((mov) => mov < 0).reduce((acc, mov) => acc + mov, 0);
+
+  labelSumIn.textContent = `${incomes}€`;
+  labelSumOut.textContent = `${Math.abs(out)}€`;
+};
+
+calcDisplaySummary(account1.movements);
+
 const createUserName = function (accs) {
   accs.forEach((acc) => {
     acc.userName = acc.owner
